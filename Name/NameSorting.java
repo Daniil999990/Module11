69336d6c4fa75a651247882109a823aec1ad01fa
Daniil999990 <dannydendi@gmail.com>
2023-04-21 03:15:38 +0300

    NameSorting

--- Name/NameSorting.java
+++ Name/NameSorting.java
@@ -9,7 +9,7 @@ public class NameSorting {
 
     public static String getOddIndexedNames(List<String> names) {
         return IntStream.range(0, names.size())
-                .filter(i -> i % 2 == 0)
+                .filter(i -> i % 2 != 0)
                 .mapToObj(i -> (i + 1) + ". " + names.get(i))
                 .collect(Collectors.joining(", "));
     }
@@ -17,7 +17,7 @@ public class NameSorting {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Ivan", "Peter", "Mary", "Eva", "Roma", "Max", "Danny", "Igor", "Slava");
         String result = getOddIndexedNames(names);
-        // виведе 1. Ivan, 3. Mary, 5. Roma, 7. Danny, 9. Slava
+        // виведе 2. Peter, 4. Eva, 6. Max, 8. Igor
         System.out.println(result);
     }
 
