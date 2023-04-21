3865864f312c4cc5835662a62f888710a4b19526
Daniil999990 <dannydendi@gmail.com>
2023-04-22 02:29:43 +0300

    NumbersAndSorting

--- Numbers/NumbersAndSorting.java
+++ Numbers/NumbersAndSorting.java
@@ -1,23 +1,18 @@
 package Numbers;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class NumbersAndSorting {
     public static void main(String[] args) {
         String[] arr = {"1, 2, 0", "4, 5"};
-        Stream<Integer> numbers = Stream.of(arr)
-                .flatMap(s -> Stream.of(s.split(", ")))
+        String result = Arrays.stream(arr)
+                .flatMap(s -> Arrays.stream(s.split(", ")))
                 .map(Integer::parseInt)
-                .sorted();
-        StringBuilder sorting = new StringBuilder();
-        numbers.forEach(num -> {
-            sorting.append(num);
-            sorting.append(", ");
-        });
-        if (sorting.length() > 2) {
-            sorting.setLength(sorting.length() - 2);
-        }
+                .sorted()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
         // виведе 0, 1, 2, 4, 5
-        System.out.println(sorting);
+        System.out.println(result);
     }
 }
